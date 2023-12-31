b741712c7d604ecc5f3f722d2a1dd74a69d5e0cb
MetaBlue <108994181+MetaBlue2000@users.noreply.github.com>
2023-12-31 15:55:34 +0100

    Master

---
+++ src/js/main.js
@@ -0,0 +1,41 @@
+const AFFICHAGE = document.getElementById("screen");
+const COMPLEXEMODE = document.getElementById("complexeMode");
+const CLEAR = document.getElementById("c");
+const CLEARE = document.getElementById("ce");
+const DIVISION = document.getElementById("division");
+const SEPT = document.getElementById("sept");
+const HUIT = document.getElementById("huit");
+const NEUF = document.getElementById("neuf");
+const MULTIPLICATION = document.getElementById("multiplication");
+const QUATRE = document.getElementById("quatre");
+const CINQ = document.getElementById("cinq");
+const SIX = document.getElementById("six");
+const SOUSTRACTION = document.getElementById("soustraction");
+const UN = document.getElementById("un");
+const DUEX = document.getElementById("deux");
+const TROIS = document.getElementById("trois");
+const ADDITION = document.getElementById("addition");
+const ZERO = document.getElementById("zero");
+const VIRGULE = document.getElementById("point");
+const EGALE = document.getElementById("exe");
+
+/*window.addEventListener(
+    "keydown",
+    function (event) {
+      let str =
+        "KeyboardEvent: key='" + event.key + "' | code='" + event.code + "'";
+        console.warn(str);
+    },
+  );
+
+window.addEventListener(
+    "keydown",
+    function (clavier_numpad){
+        if(clavier_numpad.code = Numpad*){
+            
+        }
+})*/
+
+onclick(DIVISION,SEPT,HUIT,NEUF,MULTIPLICATION,QUATRE,CINQ,SIX,SOUSTRACTION,UN,DEUX,TROIS,ADDITION,ZERO,VIRGULE){
+    
+}
